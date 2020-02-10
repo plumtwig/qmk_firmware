a354e9ca228f485c5ce63d74b0aacfe68344de29
Takeshi NODA <nodatk@gmail.com>
2020-02-11 03:51:24 +0900

    Rev2 Initial

--- keyboards/handwired/twig50/config.h
+++ keyboards/handwired/twig50/config.h
@@ -7,15 +7,16 @@
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Twig
-#define PRODUCT         Handwired50Keys
+#define PRODUCT         Handwired50Keys Rev2
 #define DESCRIPTION     4 rows 50 keys
 
 /* key matrix size */
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 7
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 14
 
-#define MATRIX_ROW_PINS { A2, A1, A0, B8, B13, B14, B15, B9 }
-#define MATRIX_COL_PINS { B6, B5, B4, B3, B2, B1, B0 }
+#define MATRIX_ROW_PINS { A2, A1, A0, B8 }
+//#define MATRIX_COL_PINS { B7, B6, B5, B4, B3, B2, B1, B0, B10, B11, A4, A5, A6, A7 }
+#define MATRIX_COL_PINS { B13, B14, B15, B9, B7, B6, B5, B4, B3, B2, B1, B0, B10, B11 }
 
 #define DEBOUNCE 8
 #define TAPPING_TERM 150
